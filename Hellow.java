bef92595ec0e4dbe2532fd448182638714d47170
vanaparthiranvitha <vanaparthiranvitha@gmail.com>
2026-08-04 11:55:04 +0530

    Add greet method to Hello class
    Refactor greeting logic and add greet method.

--- Hellow.java
+++ Hellow.java
@@ -1,8 +1,9 @@
 class Hello{
+	public static String greet(String name) {
+		return "WELCOME TO DEVOPS" + " " + name;
+	}
 	public static void main(String[]args){
-		String name=System.getenv("USERNAME_VAR");
-	if(name!=null){
-		System.out.println("Hello"+" "+name);
-	}else{
-		System.out.println("environment variable not set");
-	}}}
+		system.out.println(greet[0]));
+	}
+}
+
